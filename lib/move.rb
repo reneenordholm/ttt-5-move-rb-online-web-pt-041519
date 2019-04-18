8fea39e9de404ced0b5222497b4a57b449915f3e
Renee Nordholm <reneenordholm@gmail.com>
2019-04-18 14:37:31 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(user_input)
+  "#{user_input}".to_i - 1
+end
+
+def move(board_array, index_array, players_character = "X")
+  board_array[index_array] = players_character
+end
